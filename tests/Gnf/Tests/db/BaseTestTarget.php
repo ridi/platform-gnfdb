a5400fed005c0c545264b46eae998bc1dc686e68
js.park <js.park@ridi.com>
2019-03-14 15:18:07 +0900

    tab to space <2>

--- tests/Gnf/Tests/db/BaseTestTarget.php
+++ tests/Gnf/Tests/db/BaseTestTarget.php
@@ -5,98 +5,98 @@ use Gnf\db\base;
 
 class BaseTestTarget extends base
 {
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	public function insertId()
-	{
-	}
+    public function insertId()
+    {
+    }
 
-	public function insert_id()
-	{
-	}
+    public function insert_id()
+    {
+    }
 
-	protected function doConnect()
-	{
-	}
+    protected function doConnect()
+    {
+    }
 
-	protected function hasConnected()
-	{
-	}
+    protected function hasConnected()
+    {
+    }
 
-	public function selectDb($db)
-	{
-	}
+    public function selectDb($db)
+    {
+    }
 
-	public function select_db($db)
-	{
-	}
+    public function select_db($db)
+    {
+    }
 
-	protected function transactionBegin()
-	{
-	}
+    protected function transactionBegin()
+    {
+    }
 
-	protected function transactionCommit()
-	{
-	}
+    protected function transactionCommit()
+    {
+    }
 
-	protected function transactionRollback()
-	{
-	}
+    protected function transactionRollback()
+    {
+    }
 
-	/**
-	 * @return bool
-	 */
-	protected function configIsSupportNestedTransaction()
-	{
-		return false;
-	}
+    /**
+     * @return bool
+     */
+    protected function configIsSupportNestedTransaction()
+    {
+        return false;
+    }
 
-	protected function escapeLiteral($value)
-	{
-		if (!is_string($value)) {
-			$value = strval($value);
-		}
+    protected function escapeLiteral($value)
+    {
+        if (!is_string($value)) {
+            $value = strval($value);
+        }
 
-		return str_replace(
-			['\\', "\0", "\n", "\r", "'", '"', "\x1a"],
-			['\\\\', '\\0', '\\n', '\\r', "\\'", '\\"', '\\Z'],
-			$value
-		);
-	}
+        return str_replace(
+            ['\\', "\0", "\n", "\r", "'", '"', "\x1a"],
+            ['\\\\', '\\0', '\\n', '\\r', "\\'", '\\"', '\\Z'],
+            $value
+        );
+    }
 
-	protected function query($sql)
-	{
-	}
+    protected function query($sql)
+    {
+    }
 
-	protected function getError($handle)
-	{
-	}
+    protected function getError($handle)
+    {
+    }
 
-	protected function fetchRow($handle)
-	{
-	}
+    protected function fetchRow($handle)
+    {
+    }
 
-	protected function fetchAssoc($handle)
-	{
-	}
+    protected function fetchAssoc($handle)
+    {
+    }
 
-	protected function fetchObject($handle)
-	{
-	}
+    protected function fetchObject($handle)
+    {
+    }
 
-	protected function fetchBoth($handle)
-	{
-	}
+    protected function fetchBoth($handle)
+    {
+    }
 
-	/**
-	 * @param $handle
-	 *
-	 * @return int
-	 */
-	protected function getAffectedRows($handle)
-	{
-	}
+    /**
+     * @param $handle
+     *
+     * @return int
+     */
+    protected function getAffectedRows($handle)
+    {
+    }
 }
